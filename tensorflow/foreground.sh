9a3d7e0199cc85833ea4635af66855c87476d694
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2020-05-15 13:15:08 +0900

    Update foreground.sh

--- tensorflow/foreground.sh
+++ tensorflow/foreground.sh
@@ -1,2 +1,2 @@
-sudo npm install -g node-red node-red-contrib-tensorflow node-red-contrib-browser-utils node-red-contrib-image-output
-node-red
+npm install node-red node-red-contrib-tensorflow node-red-contrib-browser-utils node-red-contrib-image-output
+node node_modules/node-red/red.js
