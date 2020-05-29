9866b53ee008b04e0dbeb748ba2b4e9d23d839d1
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2020-05-29 16:28:31 +0900

    Update foreground.sh

--- tensorflow/foreground.sh
+++ tensorflow/foreground.sh
@@ -1,5 +1,5 @@
 mkdir ~/.node-red
 curl https://raw.githubusercontent.com/kazuhitoyokoi/katacoda-scenarios/master/tensorflow/flows.json > ~/.node-red/flows.json
 curl https://raw.githubusercontent.com/kazuhitoyokoi/katacoda-scenarios/master/tensorflow/config.json > ~/.node-red/.config.json
-npm install node-red node-red-contrib-tensorflow node-red-contrib-browser-utils node-red-contrib-image-output
+npm install node-red node-red-contrib-tensorflow node-red-contrib-image-output
 node node_modules/node-red/red.js flows.json
