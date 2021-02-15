15382d6e9534877660d083ba0eda7a40b1984418
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2021-02-15 21:19:28 +0900

    Add demo flow

--- dashboard/foreground.sh
+++ dashboard/foreground.sh
@@ -1,5 +1,2 @@
-mkdir ~/.node-red
-curl https://raw.githubusercontent.com/kazuhitoyokoi/katacoda/master/tensorflow/flows.json > ~/.node-red/flows.json
-curl https://raw.githubusercontent.com/kazuhitoyokoi/katacoda/master/tensorflow/config.json > ~/.node-red/.config.json
-npm install node-red node-red-contrib-tensorflow node-red-contrib-image-output
-node node_modules/node-red/red.js flows.json
+npm install
+npm start
