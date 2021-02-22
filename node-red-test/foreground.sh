d968e0332e53af095af6953e4625284bf0afbb57
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2021-02-22 14:56:50 +0900

    Update foreground.sh

--- node-red-test/foreground.sh
+++ node-red-test/foreground.sh
@@ -1,3 +1,3 @@
 npm install -g node-red
 npm install -g node-red-contrib-plugin-notify
-node-red
+node-red -D notify.message='This environment will be terminated within 10 minutes. Please download your flow immediately.' -D notify.options.timeout=3600000 -D notify.options.buttons[0].text='OK' -D notify.options.buttons[0].class='primary' -D notify.timer=30000
