acf1b9f610b2fc19a3612a9a51962b5c38a5b937
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2021-04-22 01:01:15 +0900

    Update foreground.sh

--- node-red/foreground.sh
+++ node-red/foreground.sh
@@ -1,3 +1,2 @@
-wget https://github.com/kazuhitoyokoi/katacoda/raw/main/node-red.zip
-unzip node-red.zip
-node node_modules/node-red/red.js -D notify.message='This environment will be terminated within 10 minutes. Please download your flow immediately.' -D notify.options.timeout=3000000 -D notify.options.buttons[0].text='OK' -D notify.options.buttons[0].class='primary' -D notify.timer=3000000
+npm install -g node-red node-red-contrib-plugin-header
+node-red -D notify.message='This environment will be terminated within 10 minutes. Please download your flow immediately.' -D notify.options.timeout=3000000 -D notify.options.buttons[0].text='OK' -D notify.options.buttons[0].class='primary' -D notify.timer=3000000
