6fc2e39bf9fb6b61e92b29a4f770d246645f4e01
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2021-07-23 11:12:16 +0900

    Update foreground.sh

--- node-red-git/foreground.sh
+++ node-red-git/foreground.sh
@@ -1,2 +1,2 @@
-npm install -g node-red@next nrlint node-red-debugger node-red-node-swagger node-red-contrib-plugin-notify
-node-red -D editorTheme.projects.enabled=true -D functionExternalModules=true
+npm install -g node-red nrlint node-red-debugger node-red-node-swagger node-red-contrib-plugin-notify
+node-red -D editorTheme.projects.enabled=true
